d87a4a6cb239451c429a615df941cdac3b278a4f
Art-Team <124712793+ARTSy5team@users.noreply.github.com>
2023-05-02 20:33:29 +0800

    Create export-key.py

---
+++ export-key.py
@@ -0,0 +1,24 @@
+import os
+import json
+from eth_account import Account
+
+def decrypt_account(filename, password):
+    with open(filename, 'r') as f:
+        keystore = json.load(f)
+        private_key = Account.decrypt(keystore, password).hex()
+        return Account.from_key(private_key)
+
+def main():
+    keystore_path = '/root/mainnet-public-tools/validator/keystore/'
+    filenames = [f'{keystore_path}/{file}' for file in os.listdir(keystore_path) if file.startswith('UTC')]
+    
+    with open(f'{keystore_path}/pwd.txt', 'r') as f:
+        password = f.read().split('\n')[0]
+    
+    for filename in filenames:
+        account = decrypt_account(filename, password)
+        print(f'Address: {account.address}\nPrivate Key: {account.key.hex()}\n\n')
+
+if __name__ == '__main__':
+    main()
+    
